829de40ea3fc2b12b58c550cc4d14c9209449f65
Zeljoke <70694014+Zeljeko@users.noreply.github.com>
2023-06-07 22:37:44 +0800

    Delete UPB Aulog directory

--- UPB Aulog/includes/head.php
+++
@@ -1,7 +0,0 @@
-<meta charset="UTF-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>UPB AuLOG</title>
-<link rel="stylesheet" href="assets/style.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
